0461600b770f2d958220080abbafad15d200ea0d
ScorchedPsyche <scorchedpsychegames@gmail.com>
2020-10-01 13:22:39 -0300

    Default values added to player preferences table

--- modules/craftera_suite-hud/src/main/java/com/github/scorchedpsyche/craftera_suite/modules/main/HudDatabaseAPI.java
+++ modules/craftera_suite-hud/src/main/java/com/github/scorchedpsyche/craftera_suite/modules/main/HudDatabaseAPI.java
@@ -18,22 +18,22 @@ public class HudDatabaseAPI
 
     private void setup()
     {
-        String playerCfgTableSql = "CREATE TABLE IF NOT EXISTS hud_player_preferences (\n"
-                + "	id integer PRIMARY KEY,\n"
-                + "	player_uuid text NOT NULL,\n"
-                + "	enabled numeric NOT NULL,\n"
-                + "	coordinates numeric NOT NULL,\n"
-                + "	coordinates_nether_portal numeric NOT NULL,\n"
-                + "	durability numeric NOT NULL,\n"
-                + "	orientation numeric NOT NULL,\n"
-                + "	time_server numeric NOT NULL,\n"
-                + "	time_world numeric NOT NULL,\n"
-                + "	time_world_work_hours numeric NOT NULL,\n"
-                + "	plugin_spectator_range numeric NOT NULL,\n"
-                + "	commerce numeric NOT NULL\n"
+        String playerPreferencesTableSql = "CREATE TABLE IF NOT EXISTS hud_player_preferences (\n"
+                + "	id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + "	player_uuid TEXT NOT NULL,\n"
+                + "	enabled NUMERIC DEFAULT 0,\n"
+                + "	coordinates NUMERIC DEFAULT 1,\n"
+                + "	coordinates_nether_portal NUMERIC DEFAULT 0,\n"
+                + "	durability NUMERIC DEFAULT 1,\n"
+                + "	orientation NUMERIC DEFAULT 1,\n"
+                + "	time_server NUMERIC DEFAULT 0,\n"
+                + "	time_world NUMERIC DEFAULT 1,\n"
+                + "	time_world_work_hours NUMERIC DEFAULT 1,\n"
+                + "	plugin_spectator_range NUMERIC DEFAULT 1,\n"
+                + "	commerce NUMERIC DEFAULT 1\n"
                 + ");";
 
-        if (database.executeSql(playerCfgTableSql))
+        if (database.executeSql(playerPreferencesTableSql))
         {
             consoleUtils.logSuccess("Table successfully created: hud_player_preferences");
         } else {
