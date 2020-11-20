fd5c18e9bd8a4e35762478805deba508c4be16b8
ScorchedPsyche <scorchedpsychegames@gmail.com>
2020-11-20 08:22:55 -0300

    CORE: ParticleUtils code moved from Baby Entities; WANDERING TRADES: fixed possible null exception; WANDERING TRADES: removed unused code.

--- modules/craftera_suite-baby_entities/src/main/java/com/github/scorchedpsyche/craftera_suite/entities/baby/listeners/EntityNamingListener.java
+++ modules/craftera_suite-baby_entities/src/main/java/com/github/scorchedpsyche/craftera_suite/entities/baby/listeners/EntityNamingListener.java
@@ -1,15 +1,12 @@
 package com.github.scorchedpsyche.craftera_suite.entities.baby.listeners;
 
-import com.github.scorchedpsyche.craftera_suite.entities.baby.CraftEraSuiteBabyEntities;
 import com.github.scorchedpsyche.craftera_suite.entities.baby.utils.EntityUtil;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
 
 public class EntityNamingListener implements Listener {
     EntityUtil entityUtil = new EntityUtil();
@@ -47,107 +44,4 @@ public class EntityNamingListener implements Listener {
             }
         }
     }
-
-//    private void convertIfValid(Player sourcePlayer, Entity targetEntity)
-//    {
-//        // Is entity valid for conversion?
-//        if ( entityUtil.isAgeable(targetEntity) )
-//        {
-//            // Valid entity. Convert to adult or baby?
-//            if ( !((Ageable) targetEntity).isAdult() )
-//            {
-//                // ADULT
-//
-//                // Checks if it's already been converted
-//                if( ((Breedable) targetEntity).getAgeLock() )
-//                {
-//                    // AgeLock is true, convert it back to adult
-//                    ((Breedable) targetEntity).setAgeLock(false);
-//                    ((Ageable) targetEntity).setAdult();
-//
-//                    if( ((Ageable) targetEntity).isAdult() ){
-//                        // Successfully converted to Adult
-//
-//                        // Remove metadata
-//                        targetEntity.removeMetadata("ces_baby/adult" , plugin);
-//
-//                        // Particles
-//                        entityUtil.spawnParticleAtEntity(
-//                                targetEntity, Particle.DAMAGE_INDICATOR, 10, 0.0001);
-//                    } else {
-//                        // Failed. Can't be an adult
-//                        sourcePlayer.sendRawMessage(
-//                                Text_CraftEra_Suite + ChatColor.RED + targetEntity.getName() + ChatColor.RESET +
-//                                        " can't be an adult." );
-//
-//                        entityUtil.spawnParticleAtEntity(
-//                                targetEntity, Particle.EXPLOSION_NORMAL,3, 0.01);
-//                    }
-//                } else {
-//                    // Hasn't been converted so the conversion is invalid.
-//                    sourcePlayer.sendRawMessage(
-//                            Text_CraftEra_Suite + ChatColor.RED + "This is a natural baby!" + ChatColor.RESET +
-//                                    " You can't convert Vanilla entities to Adult.");
-//
-//                    entityUtil.spawnParticleAtEntity(
-//                            targetEntity, Particle.EXPLOSION_NORMAL,3, 0.01);
-//                }
-//            } else {
-//                // BABY
-//
-//                // Attempts to convert entity into a Baby
-//                ((Breedable) targetEntity).setAgeLock(true);
-//                ((Ageable) targetEntity).setBaby();
-//
-//                if( !((Ageable) targetEntity).isAdult() ){
-//                    // Successfully converted to Baby
-//
-//                    // Remove metadata
-//                    targetEntity.setMetadata( "ces_baby/adult",
-//                                              new FixedMetadataValue ( plugin, "ces_baby/adult" ) );
-//
-//                    // Particles
-//                    entityUtil.spawnParticleAtEntity(targetEntity, Particle.HEART, 15);
-//                } else {
-//                    // Failed. Can't be a baby
-//                    ((Breedable) targetEntity).setAgeLock(false);
-//
-//                    sourcePlayer.sendRawMessage(
-//                            Text_CraftEra_Suite + ChatColor.RED + targetEntity.getName() + ChatColor.RESET +
-//                                    " can't be a baby." );
-//
-//                    entityUtil.spawnParticleAtEntity(
-//                            targetEntity, Particle.EXPLOSION_NORMAL,3, 0.01);
-//                }
-//            }
-//
-//            // Create packet to notify players of the conversion
-//            DataWatcher watcher = ((CraftAgeable) targetEntity).getHandle().getDataWatcher();
-//            PacketPlayOutEntityMetadata packet = new PacketPlayOutEntityMetadata(
-//                    targetEntity.getEntityId(), // Entity ID
-//                    watcher, // Data watcher which you can get by accessing a method in a NMS Entity class
-//                    false // Send All
-//            );
-//
-//            // Notify nearby players
-//            for (Player player : Bukkit.getOnlinePlayers()) {
-//                // Check if in the same dimension
-//                if( targetEntity.getWorld().getUID() == player.getWorld().getUID() )
-//                {
-//                    if( entityUtil.IsPlayerBetweenViewDistanceOfEntity(targetEntity, player) )
-//                    {
-//                        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
-//                    }
-//                }
-//            }
-//        } else {
-//            // Invalid entity for conversion
-//            sourcePlayer.sendRawMessage(
-//                    Text_CraftEra_Suite + ChatColor.RED + targetEntity.getName() + ChatColor.RESET +
-//                            " is not valid for conversion!" );
-//
-//            entityUtil.spawnParticleAtEntity(
-//                    targetEntity, Particle.EXPLOSION_NORMAL,3, 0.01);
-//        }
-//    }
 }
