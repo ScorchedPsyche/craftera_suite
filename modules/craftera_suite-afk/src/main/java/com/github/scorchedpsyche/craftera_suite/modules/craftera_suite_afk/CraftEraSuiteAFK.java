6c85f498335afbce2e68ac909789a83bd2a8b66e
Michel Arendt <kingdomx@gmail.com>
2021-06-14 11:46:08 -0300

    AFK: initial module code

---
+++ modules/craftera_suite-afk/src/main/java/com/github/scorchedpsyche/craftera_suite/modules/craftera_suite_afk/CraftEraSuiteAFK.java
@@ -0,0 +1,17 @@
+package com.github.scorchedpsyche.craftera_suite.modules.craftera_suite_afk;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CraftEraSuiteAFK extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
