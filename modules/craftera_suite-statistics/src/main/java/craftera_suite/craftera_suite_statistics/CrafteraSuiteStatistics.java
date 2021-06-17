f41e702d7bde24990ff6ff7d3b8a8f345ee7a473
ScorchedPsyche <scorchedpsychegames@gmail.com>
2021-06-17 08:19:04 -0300

    STATISTICS: initial code

---
+++ modules/craftera_suite-statistics/src/main/java/craftera_suite/craftera_suite_statistics/CrafteraSuiteStatistics.java
@@ -0,0 +1,17 @@
+package craftera_suite.craftera_suite_statistics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CrafteraSuiteStatistics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
