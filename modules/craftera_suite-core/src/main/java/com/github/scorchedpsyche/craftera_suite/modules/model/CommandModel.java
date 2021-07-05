2904a7dc624441e29f9d0d61bb072a6510481c55
ScorchedPsyche <scorchedpsychegames@gmail.com>
2021-07-05 18:20:47 -0300

    CORE: fluent API design at CommandModel for commands

--- modules/craftera_suite-core/src/main/java/com/github/scorchedpsyche/craftera_suite/modules/model/CommandModel.java
+++ modules/craftera_suite-core/src/main/java/com/github/scorchedpsyche/craftera_suite/modules/model/CommandModel.java
@@ -1,6 +1,9 @@
 package com.github.scorchedpsyche.craftera_suite.modules.model;
 
+import com.github.scorchedpsyche.craftera_suite.modules.util.ConsoleUtil;
+
 import java.util.HashMap;
+import java.util.Stack;
 
 public class CommandModel
 {
@@ -24,11 +27,89 @@ public class CommandModel
         this.playersAsSubcommands = playersAsSubcommands;
     }
 
-    public HashMap<String, CommandModel> subCommands;
+    public HashMap<String, CommandModel> subCommands = new HashMap<>();
+    Stack<String> breadcrumbs = new Stack<>();
+    @Deprecated
     private String lastKey;
     private String permission;
     private boolean playersAsSubcommands = false;
 
+    /**
+     * Adds a sibling command
+     * @param commandName Name of the command
+     * @return Fluent API builder
+     */
+    public CommandModel sibling(String commandName)
+    {
+        if( !breadcrumbs.empty() )
+        {
+            siblingRecursive(commandName, subCommands.get(breadcrumbs.firstElement()), 1);
+        } else {
+            subCommands.put(commandName, new CommandModel());
+            breadcrumbs.push(commandName);
+        }
+
+        return this;
+    }
+
+    /**
+     * Recursive method that adds the command at the proper location
+     * @param commandName Name of the command
+     */
+    private void siblingRecursive(String commandName, CommandModel childCommands, int index)
+    {
+        if( index == breadcrumbs.size() - 1 )
+        {
+            childCommands.subCommands.put(commandName, new CommandModel());
+            if(breadcrumbs.size() > 0)
+            {
+                breadcrumbs.pop();
+            }
+            breadcrumbs.push(commandName);
+        } else {
+            siblingRecursive(commandName, childCommands.subCommands.get(breadcrumbs.get(index)), ++index);
+        }
+    }
+
+    /**
+     * Adds a child command
+     * @param commandName Name of the command
+     * @return Fluent API builder
+     */
+    public CommandModel child(String commandName)
+    {
+        childRecursive(commandName, subCommands.get(breadcrumbs.firstElement()), 0);
+
+        return this;
+    }
+
+    /**
+     * Recursive method that adds the command at the proper location
+     * @param commandName Name of the command
+     */
+    private void childRecursive(String commandName, CommandModel parentCommands, int index)
+    {
+        if( index == breadcrumbs.size() - 1 )
+        {
+            parentCommands.subCommands.put(commandName, new CommandModel());
+            breadcrumbs.push(commandName);
+        } else {
+            childRecursive(commandName, parentCommands.subCommands.get(breadcrumbs.get(++index)), index);
+        }
+    }
+
+    /**
+     * Returns to the parent from the current depth
+     * @return Fluent API builder
+     */
+    public CommandModel back()
+    {
+        breadcrumbs.pop();
+
+        return this;
+    }
+
+    @Deprecated
     public CommandModel addCommand(String commandName)
     {
         if ( subCommands == null )
@@ -42,6 +123,7 @@ public class CommandModel
         return this;
     }
 
+    @Deprecated
     public CommandModel addSubcommand(String commandName)
     {
         if ( subCommands.get(lastKey).subCommands == null )
@@ -54,6 +136,7 @@ public class CommandModel
         return this;
     }
 
+    @Deprecated
     public CommandModel addSubcommands(HashMap<String, CommandModel> commands)
     {
         if ( subCommands == null )
@@ -66,6 +149,7 @@ public class CommandModel
         return this;
     }
 
+    @Deprecated
     public boolean hasSubcommands()
     {
         if( playersAsSubcommands )
